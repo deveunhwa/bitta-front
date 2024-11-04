4f7b64e3dea5989f3ab9dccb794f5579fe041956
ghtndl <leesooho0406@gmail.com>
2024-11-04 15:35:39 +0900

    Feat: Feed View

---
+++ src/pages/Feed/samplePosts.jsx
@@ -0,0 +1,40 @@
+const samplePosts = [
+    {
+      id: 1,
+      title: "First Post",
+      content: "This is the first post",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 2,
+      title: "Second Post",
+      content: "This is the second post",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      title: "Third Post",
+      content: "This is the third post",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 4,
+      title: "Fourth Post",
+      content: "This is the third post",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 5,
+      title: "Fifth Post",
+      content: "This is the third post",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 6,
+      title: "Sixth Post",
+      content: "This is the third post",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+  ];
+  
+  export default samplePosts;
