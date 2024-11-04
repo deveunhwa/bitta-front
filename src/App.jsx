8912574ab742c142ca8b499cd1e1be30e3dd04bf
ghtndl <leesooho0406@gmail.com>
2024-11-04 15:35:39 +0900

    Feat: Feed View

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Calendar1 from "./pages/Calendar1.jsx";
 import Jobpost from "./pages/JobPost.jsx";
+import Feed from './pages/Feed/Feed.jsx';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/Calendar1" element={<Calendar1 />} />
                 <Route path="/Jobpost" element={<Jobpost />} />
+                <Route path="/Feed" element={<Feed />} />
             </Routes>
         </Router>
     );
