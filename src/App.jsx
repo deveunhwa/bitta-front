1d8012c4b784c1a8c8cd702562736a9b189a573f
deveunhwa <deveunhwa@gmail.com>
2024-11-06 04:08:12 +0900

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import Calendar1 from "./pages/Calendar1.jsx";
 import Jobpost from "./pages/JobPost.jsx";
 import JobPostAddForm from "./pages/JobPostAdd/JobPostAdd.jsx";
 import Feed from './pages/Feed/Feed.jsx';
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx"
+
+// import Chat from "./pages/Chat.jsx";
+// import Register from "./pages/Register.jsx";
 
 function App() {
     return (
@@ -15,9 +20,14 @@ function App() {
                 <Route path="/Jobpost" element={<Jobpost />} />
                 <Route path="/add-job-post" element={<JobPostAddForm />} />
                 <Route path="/Feed" element={<Feed />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Signup" element={<Signup />} />
+                {/*<Route path="/Chat" element={<Chat />} />*/}
+
             </Routes>
         </Router>
     );
 }
 
 export default App;
+
